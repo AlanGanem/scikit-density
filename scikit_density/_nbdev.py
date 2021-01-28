afb067465b79e802a3ad6764bb1e6161ba407c4f
unknown <ganem.alan@hotmail.com>
2021-01-28 19:30:26 -0300

    added alpha, beta and gamma query params

--- scikit_density/_nbdev.py
+++ scikit_density/_nbdev.py
@@ -2,16 +2,25 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"EnsembleTreesDensityMixin": "01_ensemble.ipynb",
+index = {"expected_likelihood": "01_ensemble.ipynb",
+         "inverese_log_node_var": "01_ensemble.ipynb",
+         "datapoint_pdf": "01_ensemble.ipynb",
+         "datapoint_gaussian_likelihood": "01_ensemble.ipynb",
+         "AVALIBLE_NODE_AGG": "01_ensemble.ipynb",
+         "AVALIBLE_DATAPOINT_WEIGHT_FUNC": "01_ensemble.ipynb",
+         "EnsembleTreesDensityMixin": "01_ensemble.ipynb",
          "SimilarityTreeEnsemble": "01_ensemble.ipynb",
          "BaggingDensityEstimator": "01_ensemble.ipynb",
          "AdaBoostDensityEstiamtor": "01_ensemble.ipynb",
+         "KDE": "02_core.random_variable.ipynb",
          "RandomVariable": "02_core.random_variable.ipynb",
          "RVArray": "02_core.random_variable.ipynb",
          "pad_to_shape": "03_utils.ipynb",
+         "transform_similarity_weights": "03_utils.ipynb",
          "cos_sim_query": "03_utils.ipynb",
          "sample_multi_dim": "03_utils.ipynb",
-         "ctqdm": "03_utils.ipynb"}
+         "ctqdm": "03_utils.ipynb",
+         "DelegateEstimatorMixIn": "03_utils.ipynb"}
 
 modules = ["ensemble.py",
            "core/random_variable.py",
